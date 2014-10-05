7c1a62a6f111fe5658f05ece35f9b5e69dad3592
surdupetru <surdu_petru@yahoo.com>
2014-10-05 11:27:20 +0200

    remove some #define from bluetooth ...

--- device/huawei/hwp6_u06/bluetooth/bdroid_buildcfg.h
+++ device/huawei/hwp6_u06/bluetooth/bdroid_buildcfg.h
@@ -18,10 +18,8 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME   "Ascend P6"
-/*#define BTA_DISABLE_DELAY 1000  in milliseconds */
 
 #define BLE_INCLUDED TRUE
-#define BTA_GATT_INCLUDED TRUE
-#define SMP_INCLUDED TRUE
+
 
 #endif
